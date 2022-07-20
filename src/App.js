edd7d8a7ea555559192a92d2e13a1eee89290198
Jovan Trifković <jowantrifkovic@gmail.com>
2022-07-20 10:40:25 +0200

    about

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from './Components/navbar/Navbar';
+import About from './Pages/About';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/" element={<Home />} /> */}
         <Route exact path="/about" element={<About />} />
-        <Route exact path="/services" element={<Services />} />
+        {/* <Route exact path="/services" element={<Services />} /> */}
       </Routes>
     </Router>
   );
